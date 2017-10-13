2fe4c71a2165dc634ac6a7a99151cc587c9ab2f1
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-10-13 11:23:37 -0400

    added artist_id

--- migrations/20171013104311_track.js
+++ migrations/20171013104311_track.js
@@ -3,6 +3,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('track', function (table) {
     table.increments();
     table.text('name');
+    table.integer('artist_id');
   });
 };
 
